2b53df3237eb4def8c77ba5cb897662f5e93120b
Shadman1077 <shadman.khan1582@gmail.com>
2021-09-08 23:35:13 +0600

    Completed UI elements

---
+++ project/javascript/login.js
@@ -0,0 +1,26 @@
+var attempt = 3; //Variable to count number of attempts
+
+//Below function Executes on click of login button
+function validate() {
+    //valid or not
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    //taking password
+    if (username == "Shanjida" && password == "swarna") {
+        alert("Login successfully");
+        window.location = "success.html";
+        return false;
+    } else {
+        attempt--;
+        alert("Incorrect Password !! You have left " + attempt + " attempt;");
+
+        //Disabling fields after 3 attempts
+        if (attempt == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
